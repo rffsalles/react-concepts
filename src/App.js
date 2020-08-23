4aff8cfe4d20893070462d76595b82808026f465
Ricardo Salles <ricardo@unovo.com.br>
2020-08-23 06:29:36 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 import "./styles.css";
-
+import api from './services/api'
 function App() {
+
+  const [repositories,setRepositories] = useState([]);
+  useEffect(() => {
+      api.get('repositories').then(response =>{ 
+        setRepositories(response.data);
+      })
+  },[]);  
   async function handleAddRepository() {
-    // TODO
+      const response = await api.post('repositories',{
+        title:`New Repository ${Date.now()}`
+      });
+      setRepositories([...repositories,response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+      const response = await api.delete(`repositories/${id}`).then(res => {        
+        if (!res.data){
+          setRepositories(repositories.filter(repository => repository.id !== id));
+        }
+      }); 
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
+      <ul data-testid="repository-list">
+      {repositories.map(repository =>{
+                        return (<li key={repository.id}>{repository.title}
+                          <button onClick={() => handleRemoveRepository(repository.id)}>
+                            Remover
+                          </button>                        
+                        </li>
+                        )
+                    })}
+      </ul>      
     </div>
   );
 }
